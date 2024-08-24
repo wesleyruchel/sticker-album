8203c25c811a7dd8cf17c942e614767a7af3f8d7
wesleyruchel <wesleyruchel@gmail.com>
2024-08-24 20:27:26 -0300

    Creating model for application user.

---
+++ APIStickerAlbum/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Text.Json.Serialization;
+
+namespace APIStickerAlbum.Models;
+
+public class ApplicationUser : IdentityUser<int>
+{
+    public string Type { get; set; } = null!;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<EducatorsAlbum> EducatorsAlbums { get; set; } = new List<EducatorsAlbum>();
+
+    [JsonIgnore]
+    public virtual ICollection<LearnersAlbum> LearnersAlbums { get; set; } = new List<LearnersAlbum>();
+
+    [JsonIgnore]
+    public virtual ICollection<LearnersSticker> LearnersStickers { get; set; } = new List<LearnersSticker>();
+}
