fabeda7837bfb99ec3c10f9344176132197a733c
wesleyruchel <wesleyruchel@gmail.com>
2024-08-24 21:25:58 -0300

    Created service to get current user.

---
+++ APIStickerAlbum/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,9 @@
+﻿using APIStickerAlbum.Models;
+
+namespace APIStickerAlbum.Interfaces;
+
+public interface ICurrentUserService
+{
+    string GetUserId();
+    Task<ApplicationUser> GetCurrentUserAsync();
+}
---
+++ APIStickerAlbum/Services/CurrentUserService.cs
@@ -0,0 +1,31 @@
+﻿using APIStickerAlbum.Interfaces;
+using APIStickerAlbum.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace APIStickerAlbum.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public CurrentUserService(IHttpContextAccessor contextAccessor, UserManager<ApplicationUser> userManager)
+    {
+        _contextAccessor = contextAccessor;
+        _userManager = userManager;
+    }
+
+    public string GetUserId()
+    {
+        return _contextAccessor
+            .HttpContext?
+            .User?.FindFirst(ClaimTypes.NameIdentifier)
+            .Value!;
+    }
+
+    public async Task<ApplicationUser> GetCurrentUserAsync()
+    {
+        return await _userManager.FindByIdAsync(GetUserId());
+    }
+}
